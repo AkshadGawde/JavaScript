05d349e8801c248c8f5fd8bf228a993dd2002705
akshad Gawde <gawdeakshad@gmail.com>
2024-03-13 23:25:28 +0530

    Dom-project-1

--- 05_Dom.js
+++ 05_Dom.js
@@ -1,5 +1,5 @@
-// document.body.innerHTML = "hello";
-// document.title = "good job!";
+document.body.innerHTML = "hello";
+document.title = "good job!";
 
 //properties and methods
 console.log(document.title);
@@ -11,3 +11,13 @@ document.body.innerHTML = "<button> Good Job</button>";
 
 document.querySelector("button");
 console.log(document.querySelector("button")); //querySelector can get any element form the page
+
+console.log(document.querySelector("button").innerHTML);
+
+document.querySelector("button").innerHTML = "Changed Btn";
+
+document.querySelector(".js-btn");
+console.log(document.querySelector(".js-btn"));
+
+const buttonElement = document.querySelector(".js-btn");
+console.log(buttonElement);
