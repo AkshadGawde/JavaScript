d45d7032b1def2aa9ea819597e040d6deeba199e
akshad Gawde <gawdeakshad@gmail.com>
2024-03-14 23:06:07 +0530

    06_Arrays-Loops

---
+++ 06_Arrays-Loops.js
@@ -0,0 +1,10 @@
+const arr = [10, 20, 30];
+console.log(arr);
+
+console.log(arr[0]); //Displays value on index 0
+console.log(arr[1]); //Displays value on index 1
+console.log(arr[2]); //Displays value on index 2
+
+//change the value of array
+arr[0] = 99;
+console.log(arr);
---
+++ DOM-Projects/rock-paper-sissor.js
@@ -0,0 +1,1 @@
+document.querySelector('.js-score').innerHTML = ;
